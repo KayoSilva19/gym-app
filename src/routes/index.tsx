1754b6f7a45b415279024d56133a571751a36c0f
Kayo Silva <91280312+KayoSilva19@users.noreply.github.com>
2024-03-26 20:25:04 -0300

    routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ import { AppRoutes } from "./app.routes";
 export function Routes() {
   const { colors } = useTheme()
   const theme = DefaultTheme
-
   theme.colors.background = colors.gray[700]
+  
   return (
     <Box flex={1} bg='gray.700'>
       <NavigationContainer theme={theme} >
