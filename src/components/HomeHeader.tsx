c40bc9789239734cbdd77d6001e8f51938f775fa
Kayo Silva <91280312+KayoSilva19@users.noreply.github.com>
2024-03-27 10:51:58 -0300

    :sparkles: feat: Header Component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,35 @@
+import { HStack, Heading, Icon, Text, VStack, useTheme } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+
+
+import { UserPhoto } from './UserPhoto'
+import { MaterialIcons } from '@expo/vector-icons'
+
+export function HomeHeader() {
+  const { sizes } = useTheme()
+  const sizeImage = sizes[4]
+  return (
+    <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
+      <UserPhoto 
+        source={{ uri: 'https://github.com/KayoSilva19.png' }}
+        size={sizeImage} 
+        alt='Imagem de perfil'
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color='gray.100' fontSize='md'>Olá,</Text>
+        <Heading color='gray.100' fontSize='md'>Kayo</Heading>
+      </VStack>
+
+    <TouchableOpacity>
+      <Icon
+        as={MaterialIcons} 
+        name='logout'
+        color='gray.200'
+        size={4}
+      />
+    </TouchableOpacity>
+
+    </HStack>
+  )
+}
---
+++ src/components/UserPhoto.tsx
@@ -0,0 +1,18 @@
+import { IImageProps, Image } from "native-base";
+
+type ImageProps = IImageProps & {
+  size: number
+}
+
+export function UserPhoto({ size, ...rest }: ImageProps ) {
+  return (
+    <Image 
+      w={size}
+      h={size}
+      rounded='full'
+      borderWidth={2}
+      borderColor='gray.400'
+      { ...rest }
+    />
+  )
+}
